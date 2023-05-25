2b8365f6ce6e635f6f1235496cfb737009bf2b86
ihminen50 <viljami@nisula.org>
2023-05-25 18:16:12 +0300

    lopetus kerta

--- 3D Game kodarit to 2023/Assets/script/playercontrollet.cs
+++ 3D Game kodarit to 2023/Assets/script/playercontrollet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 
@@ -7,7 +8,8 @@ public class playercontrollet : MonoBehaviour
 { 
     public CharacterController Charactercontroller;
     public int moveSpeed = 5;
-
+    public float maxAngle = 70f;
+    public float minAngle = -70f;
 
 
     private float mouseVertical = 0;
@@ -24,11 +26,20 @@ public class playercontrollet : MonoBehaviour
     {
 
         mouseHorizontal += Input.GetAxis("Mouse X");
-        mouseVertical += Input.GetAxis("Mouse Y");
-        Camera.main.transform.localRotation = Quaternion.Euler(mouseHorizontal, mouseVertical,0);
+        mouseVertical -= Input.GetAxis("Mouse Y");
+        mouseVertical = Mathf.Clamp(mouseVertical, minAngle, maxAngle);
+        Camera.main.transform.localRotation = Quaternion.Euler(mouseVertical, mouseHorizontal, 0);
+
         float forwardMove = Input.GetAxis("Vertical");
         float sideMove = Input.GetAxis("Horizontal");
+        
+        
+        
         Vector3 direction = new Vector3(sideMove,0,forwardMove);
+
+        direction =  Camera.main.transform.rotation * direction;
+
+
         Charactercontroller.SimpleMove(direction * Time.deltaTime * moveSpeed);
     }
 }    
